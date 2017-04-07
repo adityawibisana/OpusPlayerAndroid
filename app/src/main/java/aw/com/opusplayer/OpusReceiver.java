63d27ba6c7493c1a4c956bc75e715603245a502e
Aditya Wibisana <aditya.wibisana@gmail.com>
2017-04-07 18:29:12 +0700

    Simplify the logic of OpusReceiver, as the handling is on the MainActivity

--- app/src/main/java/aw/com/opusplayer/OpusReceiver.java
+++ app/src/main/java/aw/com/opusplayer/OpusReceiver.java
@@ -22,40 +22,7 @@ public class OpusReceiver extends BroadcastReceiver {
         int type = bundle.getInt(OpusEvent.EVENT_TYPE, 0);
 
         OpusMessageEvent messageEvent = new OpusMessageEvent();
-
-
-        switch (type) {
-            case OpusEvent.CONVERT_FINISHED:
-                break;
-            case OpusEvent.CONVERT_FAILED:
-                break;
-            case OpusEvent.CONVERT_STARTED:
-                break;
-            case OpusEvent.RECORD_FAILED:
-                break;
-            case OpusEvent.RECORD_FINISHED:
-                break;
-            case OpusEvent.RECORD_STARTED:
-                break;
-            case OpusEvent.RECORD_PROGRESS_UPDATE:
-                break;
-            case OpusEvent.PLAY_PROGRESS_UPDATE:
-                break;
-            case OpusEvent.PLAY_GET_AUDIO_TRACK_INFO:
-                break;
-            case OpusEvent.PLAYING_FAILED:
-                break;
-            case OpusEvent.PLAYING_FINISHED:
-                break;
-            case OpusEvent.PLAYING_PAUSED:
-                break;
-            case OpusEvent.PLAYING_STARTED:
-                messageEvent.setOpusEventCode(OpusEvent.PLAYING_STARTED);
-                break;
-            default:
-                Log.d("OpusReceiver onReceive", intent.toString() + "Invalid request,discarded");
-                break;
-        }
+        messageEvent.setOpusEventCode(type);
 
         EventBus.getDefault().post(messageEvent);
     }
