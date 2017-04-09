875909e38cbdfd64442368b88a3e54c343c43670
Aditya Wibisana <aditya.wibisana@gmail.com>
2017-04-09 13:35:16 +0700

    Omit the usage of extends, as it will make EventBus also sending the message to the descendant

--- app/src/main/java/aw/com/events/OpusControllerEvent.java
+++ app/src/main/java/aw/com/events/OpusControllerEvent.java
@@ -4,5 +4,14 @@ package aw.com.events;
  * Created by Adit on 4/9/2017.
  */
 
-public class OpusControllerEvent extends OpusMessageEvent {
+public class OpusControllerEvent {
+    private int opusEventCode;
+
+    public int getOpusEventCode() {
+        return opusEventCode;
+    }
+
+    public void setOpusEventCode(int opusEventCode) {
+        this.opusEventCode = opusEventCode;
+    }
 }
